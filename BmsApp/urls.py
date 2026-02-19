cdc1f7d9a0134c6f363351ba891b5182460cf842
Jagrit Bharara <jagritbharara12@gmail.com>
2026-02-19 23:10:53 +0530

    created User1 u@company.com, implemented authentication jwt

--- BmsApp/urls.py
+++ BmsApp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.home, name='home'),
     path('budget/',views.budget, name='budget'),
     path('expenses/', views.expenses, name='expenses'),
+    
 ]
