e9b13ff4ff35bbf5d871a1a484ef3ff633bedf76
Jagrit Bharara <jagritbharara12@gmail.com>
2026-02-19 22:42:35 +0530

    Pip install and routes done

--- BmsApp/views.py
+++ BmsApp/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def dashboard(request):
+    return Response({
+        "message": "Welcome to Dashboard 🚀",
+        "user": request.user.username
+    })
 
 def home(request):
     return render(request, 'home.html')
